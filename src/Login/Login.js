0d7b60a62e0785b334e1a94ffe5be4e5f3aec3ce
Brendan Schouwerwou <bschouw@yelp.com>
2017-11-16 13:33:57 -0800

    Redirect after submitting username. Store in local storage.

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,16 +2,48 @@ import React, { Component } from 'react';
 import charactersImage from '../images/character-models.png';
 import logo from '../images/logo.png';
 import './Login.css';
+import api from '../Api';
+import { Redirect } from 'react-router-dom';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      hasUser: false
+    };
+  }
+
+  async onSubmit(e) {
+    e.preventDefault();
+    const username = this.refs.usernameInput.value;
+    try {
+      let players = await api.getResource(`players/${username}`).get();
+      window.localStorage.setItem('userName', username);
+      this.setState({ hasUser: true });
+    } catch(error) {
+      throw error;
+    }
+  }
+
+  componentDidMount() {
+    this.refs.usernameInput.focus();
+  }
+
   render() {
+    const { hasUser } = this.state;
+
+    if (hasUser) {
+      return <Redirect to='/matches' />;
+    }
+
     return (
       <div className='login-page'>
         <img src={logo} className='login-page__logo' />
         <img src={charactersImage} />
-        <form className='login-form'>
-          <input type='text' placeholder="Enter your Yelp Username" />
-          <button type='submit'>Log In</button>
+        <form onSubmit={this.onSubmit} className='login-form' ref='loginForm'>
+          <input type='text' placeholder="Enter your Yelp Username" ref='usernameInput' required='true'/>
+          <button type='submit'>Continue</button>
         </form>
       </div>
     );

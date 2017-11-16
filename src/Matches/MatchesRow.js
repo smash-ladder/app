b2c1872f41842e2e9b5c37548ca50846a4c4b39c
Minh Hai Nguyen <minhhai@yelp.com>
2017-11-16 15:13:59 -0800

    generate image in matches row

--- src/Matches/MatchesRow.js
+++ src/Matches/MatchesRow.js
@@ -4,9 +4,10 @@ import './Matches.css';
 
 class MatchesRow extends React.Component {
   render() {
+    const image = require(`../images/${this.props.player.character}.png`);
     return(
       <li className='ladder-player match-player' key={this.props.key}>
-        <img src={this.props.image} className='ladder-player__image'/>
+        <img src={image} className='ladder-player__image'/>
         <div className='match-player__name'>{this.props.player.name}<br/>Rank: {this.props.player.ranking}</div>
         <button className='add-result-button'><Link to={'/result'}>Add Result</Link></button>
       </li>

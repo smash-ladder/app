8bc6ef36ae99ba723cae025bfb8b362b6915ebb2
Brendan Schouwerwou <bschouw@yelp.com>
2017-11-17 12:58:53 -0800

    Add challenge button to send email

--- src/Matches/MatchesRow.js
+++ src/Matches/MatchesRow.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link, NavLink } from 'react-router-dom';
 import './Matches.css';
+import api from '../Api';
 
 class MatchesRow extends React.Component {
+
+  sendChallenge = async () => {
+    const player = window.localStorage.getItem('userName');
+    const opponent = this.props.player.name;
+    const body = {
+      "_links": {
+        "to" : { "href": `/players/${opponent}` },
+        "from" : { "href": `/players/${player}` }
+      }
+    };
+
+    const challengeRes = await api.getResource('ladders/ssb64-1v1/challenges');
+    await challengeRes.fetch({
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    });
+  }
+
   render() {
     const image = require(`../images/${this.props.player.character}.png`);
     return(
@@ -12,6 +34,9 @@ class MatchesRow extends React.Component {
           {this.props.player.name}<br/>
           <small>Rank: {this.props.player.rank}</small>
         </div>
+        <button onClick={this.sendChallenge}>
+          Challenge
+        </button>
         <Link to={{
             pathname: '/result',
             state: { opponent: this.props.player.name }
